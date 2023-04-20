c48a84e889a1c23fb5bd8eef00a3f1a366b67fff
Samuel Barton <sbarton@Samuels-MacBook-Pro-2.local>
2023-04-20 12:52:06 -0400

    created Brand object for backend methods

--- scraper.py
+++ scraper.py
@@ -3,21 +3,50 @@
 
 import requests
 from bs4 import BeautifulSoup
+from fuzzywuzzy import fuzz
 
-fp = open("babolat.html")
-soup = BeautifulSoup(fp, 'html.parser') 
+class Brand:
+    # creates table with racquet name as key, and list of all values
+    allowedBrands = {
+        "babolat":"https://klipperusa.com/pages/babolat-tennis-racquet-patterns"
+        }
 
+    def __init__(self, brand):
+        self.brand = brand
+        self.table = self.createTable(self.brand)
+    
+    def createTable(self, brand):
+        page = requests.get(Brand.allowedBrands[brand])
+        soup = BeautifulSoup(page.content, 'html.parser') 
 
-# creates table with racquet name as key, and list of all values
-table = {}
-rows = soup.select("table tbody tr") 
-print(rows)    
-for row in rows:
-    td_list = row.find_all("td")
-    for i in range(len(td_list)):
-       td_list[i] = td_list[i].string 
+        tb = {}
+        rows = soup.select("table tbody tr") 
+        for row in rows:
+            td_list = row.find_all("td")
+            for i in range(len(td_list)):
+                td_list[i] = td_list[i].string 
+            if len(td_list) != 8: continue
+            tb.update({td_list[0]:td_list[1:]})
+        
+        return tb
 
-    if len(td_list) != 8: continue
-    table.update({td_list[0]:td_list[1:]})
+    def getListOfModels(self, query):
+        default = self.table.keys()
+        if not query: return default
+        new = []
+        # do some algorithm stuff with query to eliminate
+        # some choices
+        # TODO
+        for model in default:
+            if fuzz.token_set_ratio(query.lower(), model.lower()) > 75:
+                new.append(model)
+        return new
+    
+    def getSpecs(self, model):
+        return self.table[model]
 
-print(table)
+babolat = Brand("babolat")
+query = "pure drove"
+print(babolat.getListOfModels(query))
+for model in babolat.getListOfModels(query):
+    print(babolat.getSpecs(model))
