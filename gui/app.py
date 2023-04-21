0c1246c810b6c12bf01c5ba5b50de297257881fb
Samuel Barton <sbarton@Samuels-MacBook-Pro-2.local>
2023-04-21 13:23:25 -0400

    finished v1 working gui app

--- gui/app.py
+++ gui/app.py
@@ -8,38 +8,62 @@ from PyQt6.QtWidgets import (
     QVBoxLayout, 
     QWidget, 
     QComboBox,
-    QListWidget
+    QListWidget,
+    QStackedLayout,
+    QTableWidget,
+    QHeaderView,
+    QTableWidgetItem
 )
 import sys
-
+from pathlib import Path
+path_root = Path(__file__).parents[1]
+sys.path.append(str(path_root))
+print(sys.path)
+import scraper as s
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Stringing Info") 
-        self.setFixedSize(QSize(400,300))
+        self.setFixedSize(QSize(650,400))
 
-        self.dropdown = QComboBox()
-        self.dropdown.addItems(["Babolat","Head","Wilson","Yonex"])
-        self.dropdown.currentIndexChanged.connect(self.brandChanged)
-
+        self.brand = s.Brand("babolat")
+        
         self.label = QLabel("Select Brand:")
         self.label.setAlignment(Qt.AlignmentFlag.AlignRight)
+
+        keys = []
+        for key in self.brand.allowedBrands.keys():
+            keys.append(key.capitalize())
+        self.dropdown = QComboBox()
+        self.dropdown.addItems(keys)
+        self.dropdown.currentTextChanged.connect(self.brandChanged)
 
         self.input = QLineEdit()
         self.input.returnPressed.connect(self.returnPressed)
 
         self.models = QListWidget()
-        self.models.addItems(["1","2","3","4"])
         self.models.itemClicked.connect(self.modelClicked)
+
+        self.specs = QTableWidget(1,8)
+        self.specs.setHorizontalHeaderLabels(["Model","Tension","Length","Pattern","Skip M Holes","Tie Off M","Start C","Tie Off C"])
+        self.specs.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.ResizeToContents)      
+        self.specs.verticalHeader().setVisible(False)
+        
+        self.models.addItems(self.brand.getListOfModels(""))
 
         layout = QVBoxLayout()
         brandLayout = QHBoxLayout()
         brandLayout.addWidget(self.label)
         brandLayout.addWidget(self.dropdown)
         layout.addLayout(brandLayout)
+        
         layout.addWidget(self.input)
-        layout.addWidget(self.models)
+
+        self.stackLayout = QStackedLayout()
+        self.stackLayout.addWidget(self.models)
+        self.stackLayout.addWidget(self.specs)
+        layout.addLayout(self.stackLayout)
 
         layout.setContentsMargins(10,5,10,10)
         layout.setSpacing(5)
@@ -48,17 +72,26 @@ class MainWindow(QMainWindow):
 
         # Set the central widget of the Window.
         self.setCentralWidget(container)
+
     # drop down picking brand handling
-    def brandChanged(self, i):
-        print(i)
+    def brandChanged(self, b):
+        self.brand = s.Brand(b.lower())
+        self.stackLayout.setCurrentIndex(0)
+        self.models.clear()
+        self.models.addItems(self.brand.getListOfModels(""))
     # handling search query
     def returnPressed(self):
-        print(self.input.text())
+        self.models.clear()
+        self.models.addItems(self.brand.getListOfModels(self.input.text()))
+        self.stackLayout.setCurrentIndex(0)
         self.input.setText("")
 
     # for selecting model to show specs for
     def modelClicked(self,item):
-        print("clicked",item.text())
+        self.stackLayout.setCurrentIndex(1)
+        self.specs.setItem(0,0, QTableWidgetItem(item.text()))
+        for i in range(1,8):
+            self.specs.setItem(0,i,QTableWidgetItem(self.brand.getSpecs(item.text())[i-1]))
 
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
